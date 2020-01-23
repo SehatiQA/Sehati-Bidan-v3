978a53e68a862fa6ee3a331c5dfbf95717dad2c1
sehatiqa <sehatiqa@gmail.com>
2020-01-23 16:21:12 +0700

    add Login_19

--- Test Suites/Login.ts
+++ Test Suites/Login.ts
@@ -11,6 +11,12 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <testSuiteGuid>679567d4-ba95-4e11-8888-16470d1a2e64</testSuiteGuid>
    <testCaseLink>
+      <guid>beb9cbce-bcdf-4b88-a23b-9335237de928</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login_19</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
       <guid>d88a12f4-b54e-48a2-b5b1-a429e8da32ce</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
