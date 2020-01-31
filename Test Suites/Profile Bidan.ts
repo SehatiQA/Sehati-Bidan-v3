f22e0657451d6b0e8b751940cb1c842d0ab9cc7c
sehatiqa <sehatiqa@gmail.com>
2020-01-31 16:49:53 +0700

    rudi : - add profile bidan 15, 16, and 27

---
+++ Test Suites/Profile Bidan.ts
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Profile Bidan</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>e5d0d973-3a03-4f99-9271-bc895eed4701</testSuiteGuid>
+   <testCaseLink>
+      <guid>42892af8-75ac-4b53-81ae-79a07ea7a465</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ProfileBidan_15</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8efcfb55-a863-4299-ad4c-507bd8d8073d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ProfileBidan_16</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>905d4cf2-11cf-40d0-9342-7d0b44fcdbeb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ProfileBidan_27</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
