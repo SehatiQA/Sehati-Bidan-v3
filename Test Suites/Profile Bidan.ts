9ed1431f0e4c456a4b4d3e34164aaad25f781253
sehatiqa <sehatiqa@gmail.com>
2020-02-06 18:12:36 +0700

    move tc_04,05,06 to latest execution due to mobile.startapplication = false

--- Test Suites/Profile Bidan.ts
+++ Test Suites/Profile Bidan.ts
@@ -11,24 +11,6 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <testSuiteGuid>e5d0d973-3a03-4f99-9271-bc895eed4701</testSuiteGuid>
    <testCaseLink>
-      <guid>0c4d1554-e9cb-42d6-a2dd-7d7e0168317e</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/ProfileBidan_04</testCaseId>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>8c0e1a2d-b790-4d29-a784-8db16ff5b091</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/ProfileBidan_05</testCaseId>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>02cd50f7-2fed-4224-8ac1-9fcd178ca5e2</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/ProfileBidan_06</testCaseId>
-   </testCaseLink>
-   <testCaseLink>
       <guid>42892af8-75ac-4b53-81ae-79a07ea7a465</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
@@ -69,5 +51,23 @@
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/ProfileBidan_42</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0c4d1554-e9cb-42d6-a2dd-7d7e0168317e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ProfileBidan_04</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8c0e1a2d-b790-4d29-a784-8db16ff5b091</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ProfileBidan_05</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>02cd50f7-2fed-4224-8ac1-9fcd178ca5e2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ProfileBidan_06</testCaseId>
    </testCaseLink>
 </TestSuiteEntity>
