7d404f5aab50b76144c19d22b79d4a193d448a5f
sehatiqa <sehatiqa@gmail.com>
2020-02-07 10:38:46 +0700

    rudi : - add onboarding_07

---
+++ Test Suites/Onboarding.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Onboarding</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>3f1e638b-6e26-459d-bb6a-ff5382f1ec24</testSuiteGuid>
+   <testCaseLink>
+      <guid>767f7186-b10d-48b3-8538-951183107828</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Onboarding_07</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
