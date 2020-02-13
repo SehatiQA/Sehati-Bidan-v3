d22485ce18e5a38ea4c428ac139979273e74f13a
sehatiqa <sehatiqa@gmail.com>
2020-02-13 16:51:05 +0700

    rudi : - add lupa kata sandi 05

---
+++ Test Suites/Lupa Kata Sandi.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Lupa Kata Sandi</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>5901409e-9414-4274-86a5-3b97ec49beb8</testSuiteGuid>
+   <testCaseLink>
+      <guid>379b63c8-219e-4917-8633-1f693d6b202a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/LupaKataSandi_05</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
