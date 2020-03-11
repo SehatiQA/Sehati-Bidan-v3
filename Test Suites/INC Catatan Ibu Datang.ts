ce047111823c824a351f30046cd81c8f5ec5c5d8
sehatiqa <sehatiqa@gmail.com>
2020-03-11 14:41:15 +0700

    rudi : - add INC Catatan Ibu Datang Testsuite

---
+++ Test Suites/INC Catatan Ibu Datang.ts
@@ -0,0 +1,43 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>INC Catatan Ibu Datang</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>726757d2-e460-4b2b-b806-f2840d7c256b</testSuiteGuid>
+   <testCaseLink>
+      <guid>5bd40ef2-927b-40f5-99d8-9f1673514e39</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CatatanIbuDatang_21</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>331f7e3d-0320-4937-a68d-ea3187a7099e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CatatanIbuDatang_23</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c256b9ed-82ba-49da-8ab4-9258dcbdd671</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CatatanIbuDatang_24</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ea397f0d-44f7-44c4-bd81-020a5ebd807f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CatatanIbuDatang_44</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a48ea6bf-cbd2-40b6-b9d9-433c30db76a3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CatatanIbuDatang_45</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
