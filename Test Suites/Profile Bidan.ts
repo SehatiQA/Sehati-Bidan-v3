5406f24bf731e4b24f0083ca6b648607698e72d2
sehatiqa <sehatiqa@gmail.com>
2020-03-23 18:23:21 +0700

    rudi : - remove profile bidan 15,16,27,29,30,35,42,55 from test suite

--- Test Suites/Profile Bidan.ts
+++ Test Suites/Profile Bidan.ts
@@ -11,48 +11,6 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <testSuiteGuid>e5d0d973-3a03-4f99-9271-bc895eed4701</testSuiteGuid>
    <testCaseLink>
-      <guid>42892af8-75ac-4b53-81ae-79a07ea7a465</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/ProfileBidan_15</testCaseId>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>8efcfb55-a863-4299-ad4c-507bd8d8073d</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/ProfileBidan_16</testCaseId>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>905d4cf2-11cf-40d0-9342-7d0b44fcdbeb</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/ProfileBidan_27</testCaseId>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>6c7d3d58-39cd-4161-936b-5e793d2ba9e1</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/ProfileBidan_29</testCaseId>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>0bddaa7c-2068-403c-a0ea-a90a5d756942</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/ProfileBidan_30</testCaseId>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>129d1918-ace5-4dbd-987a-f30c09843fe1</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/ProfileBidan_35</testCaseId>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>4ce605bf-f5fd-44d4-9383-9f2c53a9a867</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/ProfileBidan_42</testCaseId>
-   </testCaseLink>
-   <testCaseLink>
       <guid>0c4d1554-e9cb-42d6-a2dd-7d7e0168317e</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
@@ -69,11 +27,5 @@
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/ProfileBidan_06</testCaseId>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>84c933b0-3aa3-4159-9908-a443c62d5c2f</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/ProfileBidan_55</testCaseId>
    </testCaseLink>
 </TestSuiteEntity>
